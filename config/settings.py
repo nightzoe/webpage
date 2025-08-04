7f34b160a309227bbfe4ee056cba606d90312b5e
sarawut <djavaarkth57@gmail.com>
2025-08-04 11:16:36 +0700

    setting.py

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2v8d%k_l(a_*p=)29*m2f40_oihni@=!-8@j!_u9lj1p#6$yr8'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-2v8d%k_l(a_*p=)29*m2f40_oihni@=!-8@j!_u9lj1p#6$yr8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
